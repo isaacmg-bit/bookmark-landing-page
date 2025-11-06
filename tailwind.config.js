a936433591d3ec3be94cda0e1fdcc9d7a468e79a
Isaac Malagón <isaacmalagongarcia@gmail.com>
2025-11-06 14:48:58 +0100

    todos los botones convertidos a tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5368df",
+        primary2: "#242946",
+        secondary: "#fa5757",
+        dark: "#242a45",
+        gray: "#9194a1",
+        lightGray: "#f7f7f7",
+        white: "#ffffff",
+        shadow: "rgba(128, 128, 128, 0.199)",
+      },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+      fontSize: {
+        h1: "1.5rem",
+        h2: "1.9rem",
+        p: "0.95rem",
+        "p-desktop": "1.10rem",
+        "h1-desktop": "2rem",
+        "h2-desktop": "3rem",
+      },
+      fontWeight: {
+        h1: "500",
+        h2: "500",
+        p: "400",
+        buttons: "700",
+      },
+      lineHeight: {
+        p: "1.6rem",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1440px",
+      },
+      boxShadow: {
+        base: "0 0.625rem 0.938rem 0 rgba(128, 128, 128, 0.199)",
+      },
+    },
+  },
+};
